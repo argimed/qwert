ed15f0804d3e0b6ae3d8ab36e4b7f77fbc2f78e4
a.alkhanov <a.alkhanov@ngrsoftlab.ru>
2024-08-03 20:21:42 +0300

    йцщгфтснгзы

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,3 +1,19 @@
-from base import Base, engine
+from base import Base, engine, SessionLocal, User
 
 Base.metadata.create_all(bind=engine)
+
+db = SessionLocal()
+users = [
+    User(login='pavel', email='a@gmail.com', hashed_password='password1'),
+    User(login='yura', email='b@gmail.com', hashed_password='password2'),
+    User(login='oleg', email='ybwtp@gmail.com', hashed_password='password3'),
+]
+
+for user in users:
+    db.add(user)
+
+db.commit()
+
+db.close()
+
+print("Database initialized with users.")
